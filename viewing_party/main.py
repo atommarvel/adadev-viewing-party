228b4c7dde53fa026910253b080b8c4ebca0cfa0
atommarvel <atom.raiff@gmail.com>
2021-05-01 17:32:16 -0400

    add_to_watched implementation also added param types for my own sanity

--- viewing_party/main.py
+++ viewing_party/main.py
@@ -1,4 +1,9 @@
-def create_movie(title, genre, rating):
+def create_movie(title:str, genre:str, rating:float):
     if not title or not genre or not rating:
         return None
     return { "title":title, "genre":genre, "rating":rating }
+
+# Add the given movie to list found at user_data.watched
+def add_to_watched(user_data: dict, movie: dict):
+    user_data["watched"].append(movie)
+    return user_data
