ec3009bbb578a3c02c0f7a3c4b860cb3de1aebea
atommarvel <atom.raiff@gmail.com>
2021-05-01 23:53:11 -0400

    append_to_user_data feels like overkill

--- viewing_party/main.py
+++ viewing_party/main.py
@@ -5,15 +5,12 @@ def create_movie(title: str, genre: str, rating: float):
         return None
     return { "title":title, "genre":genre, "rating":rating }
 
-# Add the given movie to list found at user_data.watched
 def add_to_watched(user_data: dict, movie: dict):
-    return append_to_user_data(user_data, movie, "watched")
+    user_data["watched"].append(movie)
+    return user_data
 
 def add_to_watchlist(user_data: dict, movie: dict):
-    return append_to_user_data(user_data, movie, "watchlist")
-
-def append_to_user_data(user_data: dict, movie: dict, key: str):
-    user_data[key].append(movie)
+    user_data["watchlist"].append(movie)
     return user_data
 
 def watch_movie(user_data: dict, title: str):
